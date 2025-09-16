e9a9987829224714ef785f4b567dc772142e974b
Guilhermeazevedo1 <guilhermeazevedo_2603@hotmail.com>
2025-09-15 21:52:47 -0300

    atualização

--- app.py
+++ app.py
@@ -23,11 +23,9 @@ if st.button("Gerar resposta"):
 
         try:
             
-            api_key = os.getenv("GROQ_API_KEY")
-            
             # Inicializa o modelo
             llm = ChatGroq(
-                groq_api_key= api_key,
+                groq_api_key= os.getenv("GROQ_API_KEY"),
                 model_name= "deepseek-r1-distill-llama-70b"
             )
 
